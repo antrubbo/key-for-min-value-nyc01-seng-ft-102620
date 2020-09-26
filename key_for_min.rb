8e30201506a4405da53bc5b9bc24bff8e2636d09
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-09-26 20:20:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,5 +4,11 @@
 def key_for_min_value(name_hash)
   count = 0
   name_hash.each do |key, value|
-    if 
+    if value > name_hash[count]
+      name_hash[key].slice
+      count += 1 
+    else
+      return key
+    end
+  end
 end
