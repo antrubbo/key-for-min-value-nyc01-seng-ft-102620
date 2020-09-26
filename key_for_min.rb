26a87ef0936b7ddcfe6fc3ed982e273c8bf9c94f
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-09-26 21:05:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  binding.pry
   lowest_key = nil 
   lowest_value = nil 
   name_hash.each do |name, value|
-    binding.pry 
     if lowest_value == nil || value < lowest_value
       lowest_value = value
       lowest_key = name
